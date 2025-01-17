7e8db19eb584f54d5b3be7b87bbf98dcff6376dc
Gabriel <gvsilva7767@gmail.com>
2025-01-17 16:34:07 -0400

    login and register screen

---
+++ lib/views/auth/login.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Title(
+                color: Colors.black,
+                child: const Text('Celta',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              const SizedBox(height: 60.0),
+              Container(
+                  height: 500,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: Color.fromARGB(0, 8, 12, 12)),
+                    color: Color.fromARGB(0, 227, 227, 227),
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  padding: const EdgeInsets.all(20),
+                  child: Column(children: [
+                    const SizedBox(height: 70.0),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                        
+                      ),
+                    ),
+                    const SizedBox(height: 30.0),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.password),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(Colors.black),
+                          minimumSize:MaterialStatePropertyAll(Size(600,60))
+                          ),
+                      child: const Text('Entrar',
+                      style:TextStyle(
+                        color: Colors.white,
+                      )),
+                    ),
+                    const SizedBox(height: 20),
+                     TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          "/register");
+                      },
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 34, 75, 152)),
+                          minimumSize:MaterialStatePropertyAll(Size(600,60))
+                          ),
+                      child: const Text('Registrar-se',
+                      style:TextStyle(
+                        color: Colors.white,
+                      )),
+                    ),
+                  ])),
+            ],
+          ),
+        ));
+  }
+}
